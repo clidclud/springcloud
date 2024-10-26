c092c587e9b8352fd53dc6943c02ddea312cd2d8
clidclud <clidclud@gmail.com>
2024-10-26 17:35:34 +0500

    hw 10 done

---
+++ serviceb/src/test/java/com/example/ProductControllerTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnHelloServiceB() throws Exception {
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Service B"));
+    }
+}
